9794799f3d5491b8d91976a1602e4b2e50999233
Alex Merkushyn <amerkushin@gmail.com>
2022-05-25 02:56:58 +0300

    Validation password script

--- password-validator.sh
+++ password-validator.sh
@@ -1,9 +1,27 @@
-#! /bin/bash
+#1 /bin/bash
+
+# _______-------- FUNCTION FROM PASSWORD VALIDATOR --------_______
+
 function minCharacters {
-    if [ ${#1} -le 10 ]
-    then
-        echo "1" 
-    else
-        echo "0" 
+    RED='\033[0;31m' # red color 
+    GREEN='\033[0;32m' # green color
+
+    # ---- Check for a minimum of 10 characters ----
+    if [[ ${#1} -le 10 ]];then 
+        echo -e "$RED !!! Password length should be 10 charactore !!!"
+        return 1
+    fi
+    # ---- Check using regex contain both alphabet and number, small and capital case letters ----
+    temp=`echo $1 | grep "[A-Z]" | grep "[a-z]" | grep "[0-9]"`
+
+    # ---- Checker if last command failed, password not valid ----
+    if [[ $? -ne 0 ]];then
+        echo -e "$RED !!! Password Must contain upparcase ,lowecase and number !!!"
+        return 1
+    else # --- if not failed, password valid ----
+        echo -e "$GREEN --- Your password is valid ---"
+        return 0
     fi
 }
+
+echo "$(minCharacters $1)"
